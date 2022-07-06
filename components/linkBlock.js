4d6ef4e9fa3d4a5d5bfb7110a7d3cfe53603c519
Domnewmarch <dom.newmarch@parall.ax>
2022-07-06 18:22:40 +0100

    add key to prop

--- components/linkBlock.js
+++ components/linkBlock.js
@@ -20,7 +20,7 @@ export default function LinkContainer() {
   return (
     <>
       {blocks.map(block => (
-        <div className="mt-8 w-full overflow-hidden">
+        <div key={blocks.title} className="mt-8 w-full overflow-hidden">
           <h6 className="my-2 w-full text-center font-semibold text-gray-400">{block.title}</h6>
           {block.links && (
             <ul className="flex flex-col items-center space-y-4">
