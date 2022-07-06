815299ef52f31b47ba6bdf7994845f43ae4de353
Domnewmarch <dom.newmarch@parall.ax>
2022-07-06 18:28:23 +0100

    update links

--- components/link.js
+++ components/link.js
@@ -1,7 +1,7 @@
 export default function Link({ card }) {
   return (
     <li className="w-full">
-      <a target="_blank" rel="noreferrer" href={card.href}>
+      <a rel="norefferer" target="_blank" href={card.href}>
         <div className="bg-black hover:bg-dark duration-default py-4 rounded-xl w-full">
           <p className="text-center text-white w-fit mx-auto">{card.text}</p>
         </div>
