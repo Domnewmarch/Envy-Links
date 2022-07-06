c6838eecad78df7d3a7faf3bede273fb303f88de
Domnewmarch <dom.newmarch@parall.ax>
2022-07-06 18:31:13 +0100

    remove target="_blank"

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
-      <a rel="norefferer" target="_blank" href="https://www.envydigital-webdesign.co.uk">
+      <a rel="norefferer" href="https://www.envydigital-webdesign.co.uk">
         <img className=" w-2/5 items-center m-auto" src="./img/envy-text.png" alt="" />
       </a>
     </div>
