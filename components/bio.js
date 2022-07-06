c20c44cffd80b7058d0e5966f3302df3e3dcfa3c
Domnewmarch <dom.newmarch@parall.ax>
2022-07-06 22:08:00 +0100

    bio updates

--- components/bio.js
+++ components/bio.js
@@ -6,15 +6,18 @@ export default function Bio() {
       <h1 className="w-fit text-3xl">Dom Newmarch</h1>
 
       {/* Bio section / use react quill */}
-      <p className="">Owner // Envy Digital</p>
-      <p className="">Frontend Developer // Parallax</p>
+      <p className="">
+        Owner ~ <em className="font-semibold envy-blue">Envy Digital</em>
+      </p>
+      <p className="">Frontend Developer ~ Parallax</p>
 
       {/* quick contact links */}
       <BioContact />
 
       {/* Tagline  */}
       <p>
-        Transform <em className="font-semibold">your</em> digital presence
+        <em className="font-semibold envy-blue">Design // </em> <em className="font-semibold envy-blue">Develop // </em>
+        <em className="font-semibold envy-blue">Results </em>
       </p>
     </div>
   )
